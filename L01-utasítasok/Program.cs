6cc89e3d28b6f128e0d1c1025aca6b6ea47e015c
schlemmergabor <schlemmer.gabor@nik.uni-obuda.hu>
2024-09-19 18:20:13 +0200

    1. feladat - OK

---
+++ L01-utasítasok/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace L01_utasítasok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1. feladat
+            // Console.WriteLine(); <- kiírás után új sor
+            // Console.Write(); <- kiírás után nincs új sor
+            Console.WriteLine("Hello World!");
+            #endregion
+        }
+    }
+}
